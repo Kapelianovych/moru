65df4789421405c7e1f60b38c20ca34fe5843185
YevhenKap <kapelianovych.y.v@gmail.com>
2022-09-27 16:34:31 +0300

    Replace JSX.Node with JSX.Element.

--- jsx-runtime.d.ts
+++ jsx-runtime.d.ts
@@ -2,12 +2,12 @@ import { Component, Fragment, WithChildren } from "./index.js";
 
 export function jsx(
   tag: string | Component,
-  options: Record<string, any> & { readonly children?: JSX.Node }
-): JSX.Node;
+  options: Record<string, any> & { readonly children?: JSX.Element }
+): JSX.Element;
 
 export function jsxs(
   tag: string | Component,
   options: WithChildren<Record<string, any>>
-): JSX.Node;
+): JSX.Element;
 
 export { Fragment };
