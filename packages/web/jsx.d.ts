2cd6cddd9c2c946e9e9c3fcadd7eb85036d005ea
Yevhen Kapelianovych <kapelianovych.y.v@protonmail.com>
2023-07-08 13:40:02 +0300

    Add rest of the ARIA attributes.

--- packages/web/jsx.d.ts
+++ packages/web/jsx.d.ts
@@ -511,8 +511,319 @@ declare module "moru" {
       readonly ref?: (node: B) => void;
     }
 
+    // All the WAI-ARIA 1.1 attributes from https://www.w3.org/TR/wai-aria-1.1/
     interface AriaAttributes {
-      readonly role?: AttributeValue<string>;
+      /** Identifies the currently active element when DOM focus is on a composite widget, textbox, group, or application. */
+      readonly "aria-activedescendant"?: AttributeValue<string>;
+      /** Indicates whether assistive technologies will present all, or only parts of, the changed region based on the change notifications defined by the aria-relevant attribute. */
+      readonly "aria-atomic"?: AttributeValue<boolean | "false" | "true">;
+      /**
+       * Indicates whether inputting text could trigger display of one or more predictions of the user's intended value for an input and specifies how predictions would be
+       * presented if they are made.
+       */
+      readonly "aria-autocomplete"?: AttributeValue<
+        "none" | "inline" | "list" | "both"
+      >;
+      /** Indicates an element is being modified and that assistive technologies MAY want to wait until the modifications are complete before exposing them to the user. */
+      readonly "aria-busy"?: AttributeValue<boolean | "false" | "true">;
+      /**
+       * Indicates the current "checked" state of checkboxes, radio buttons, and other widgets.
+       * @see aria-pressed @see aria-selected.
+       */
+      readonly "aria-checked"?: AttributeValue<
+        boolean | "false" | "mixed" | "true"
+      >;
+      /**
+       * Defines the total number of columns in a table, grid, or treegrid.
+       * @see aria-colindex.
+       */
+      readonly "aria-colcount"?: AttributeValue<
+        Exclude<AttributeLiteral, boolean>
+      >;
+      /**
+       * Defines an element's column index or position with respect to the total number of columns within a table, grid, or treegrid.
+       * @see aria-colcount @see aria-colspan.
+       */
+      readonly "aria-colindex"?: AttributeValue<
+        Exclude<AttributeLiteral, boolean>
+      >;
+      /**
+       * Defines the number of columns spanned by a cell or gridcell within a table, grid, or treegrid.
+       * @see aria-colindex @see aria-rowspan.
+       */
+      readonly "aria-colspan"?: AttributeValue<
+        Exclude<AttributeLiteral, boolean>
+      >;
+      /**
+       * Identifies the element (or elements) whose contents or presence are controlled by the current element.
+       * @see aria-owns.
+       */
+      readonly "aria-controls"?: AttributeValue<string>;
+      /** Indicates the element that represents the current item within a container or set of related elements. */
+      readonly "aria-current"?: AttributeValue<
+        | boolean
+        | "false"
+        | "true"
+        | "page"
+        | "step"
+        | "location"
+        | "date"
+        | "time"
+      >;
+      /**
+       * Identifies the element (or elements) that describes the object.
+       * @see aria-labelledby
+       */
+      readonly "aria-describedby"?: AttributeValue<string>;
+      /**
+       * Identifies the element that provides a detailed, extended description for the object.
+       * @see aria-describedby.
+       */
+      readonly "aria-details"?: AttributeValue<string>;
+      /**
+       * Indicates that the element is perceivable but disabled, so it is not editable or otherwise operable.
+       * @see aria-hidden @see aria-readonly.
+       */
+      readonly "aria-disabled"?: AttributeValue<boolean | "false" | "true">;
+      /**
+       * Identifies the element that provides an error message for the object.
+       * @see aria-invalid @see aria-describedby.
+       */
+      readonly "aria-errormessage"?: AttributeValue<string>;
+      /** Indicates whether the element, or another grouping element it controls, is currently expanded or collapsed. */
+      readonly "aria-expanded"?: AttributeValue<boolean | "false" | "true">;
+      /**
+       * Identifies the next element (or elements) in an alternate reading order of content which, at the user's discretion,
+       * allows assistive technology to override the general default of reading in document source order.
+       */
+      readonly "aria-flowto"?: AttributeValue<string>;
+      /** Indicates the availability and type of interactive popup element, such as menu or dialog, that can be triggered by an element. */
+      readonly "aria-haspopup"?: AttributeValue<
+        | boolean
+        | "false"
+        | "true"
+        | "menu"
+        | "listbox"
+        | "tree"
+        | "grid"
+        | "dialog"
+      >;
+      /**
+       * Indicates whether the element is exposed to an accessibility API.
+       * @see aria-disabled.
+       */
+      readonly "aria-hidden"?: AttributeValue<boolean | "false" | "true">;
+      /**
+       * Indicates the entered value does not conform to the format expected by the application.
+       * @see aria-errormessage.
+       */
+      readonly "aria-invalid"?: AttributeValue<
+        boolean | "false" | "true" | "grammar" | "spelling"
+      >;
+      /** Indicates keyboard shortcuts that an author has implemented to activate or give focus to an element. */
+      readonly "aria-keyshortcuts"?: AttributeValue<string>;
+      /**
+       * Defines a string value that labels the current element.
+       * @see aria-labelledby.
+       */
+      readonly "aria-label"?: AttributeValue<string>;
+      /**
+       * Identifies the element (or elements) that labels the current element.
+       * @see aria-describedby.
+       */
+      readonly "aria-labelledby"?: AttributeValue<string>;
+      /** Defines the hierarchical level of an element within a structure. */
+      readonly "aria-level"?: AttributeValue<
+        Exclude<AttributeLiteral, boolean>
+      >;
+      /** Indicates that an element will be updated, and describes the types of updates the user agents, assistive technologies, and user can expect from the live region. */
+      readonly "aria-live"?: AttributeValue<"off" | "assertive" | "polite">;
+      /** Indicates whether an element is modal when displayed. */
+      readonly "aria-modal"?: AttributeValue<boolean | "false" | "true">;
+      /** Indicates whether a text box accepts multiple lines of input or only a single line. */
+      readonly "aria-multiline"?: AttributeValue<boolean | "false" | "true">;
+      /** Indicates that the user may select more than one item from the current selectable descendants. */
+      readonly "aria-multiselectable"?: AttributeValue<
+        boolean | "false" | "true"
+      >;
+      /** Indicates whether the element's orientation is horizontal, vertical, or unknown/ambiguous. */
+      readonly "aria-orientation"?: AttributeValue<"horizontal" | "vertical">;
+      /**
+       * Identifies an element (or elements) in order to define a visual, functional, or contextual parent/child relationship
+       * between DOM elements where the DOM hierarchy cannot be used to represent the relationship.
+       * @see aria-controls.
+       */
+      readonly "aria-owns"?: AttributeValue<string>;
+      /**
+       * Defines a short hint (a word or short phrase) intended to aid the user with data entry when the control has no value.
+       * A hint could be a sample value or a brief description of the expected format.
+       */
+      readonly "aria-placeholder"?: AttributeValue<string>;
+      /**
+       * Defines an element's number or position in the current set of listitems or treeitems. Not required if all elements in the set are present in the DOM.
+       * @see aria-setsize.
+       */
+      readonly "aria-posinset"?: AttributeValue<
+        Exclude<AttributeLiteral, boolean>
+      >;
+      /**
+       * Indicates the current "pressed" state of toggle buttons.
+       * @see aria-checked @see aria-selected.
+       */
+      readonly "aria-pressed"?: AttributeValue<
+        boolean | "false" | "mixed" | "true"
+      >;
+      /**
+       * Indicates that the element is not editable, but is otherwise operable.
+       * @see aria-disabled.
+       */
+      readonly "aria-readonly"?: AttributeValue<boolean | "false" | "true">;
+      /**
+       * Indicates what notifications the user agent will trigger when the accessibility tree within a live region is modified.
+       * @see aria-atomic.
+       */
+      readonly "aria-relevant"?: AttributeValue<
+        | "additions"
+        | "additions removals"
+        | "additions text"
+        | "all"
+        | "removals"
+        | "removals additions"
+        | "removals text"
+        | "text"
+        | "text additions"
+        | "text removals"
+      >;
+      /** Indicates that user input is required on the element before a form may be submitted. */
+      readonly "aria-required"?: AttributeValue<boolean | "false" | "true">;
+      /** Defines a human-readable, author-localized description for the role of an element. */
+      readonly "aria-roledescription"?: AttributeValue<string>;
+      /**
+       * Defines the total number of rows in a table, grid, or treegrid.
+       * @see aria-rowindex.
+       */
+      readonly "aria-rowcount"?: AttributeValue<
+        Exclude<AttributeLiteral, boolean>
+      >;
+      /**
+       * Defines an element's row index or position with respect to the total number of rows within a table, grid, or treegrid.
+       * @see aria-rowcount @see aria-rowspan.
+       */
+      readonly "aria-rowindex"?: AttributeValue<
+        Exclude<AttributeLiteral, boolean>
+      >;
+      /**
+       * Defines the number of rows spanned by a cell or gridcell within a table, grid, or treegrid.
+       * @see aria-rowindex @see aria-colspan.
+       */
+      readonly "aria-rowspan"?: AttributeValue<
+        Exclude<AttributeLiteral, boolean>
+      >;
+      /**
+       * Indicates the current "selected" state of various widgets.
+       * @see aria-checked @see aria-pressed.
+       */
+      readonly "aria-selected"?: AttributeValue<boolean | "false" | "true">;
+      /**
+       * Defines the number of items in the current set of listitems or treeitems. Not required if all elements in the set are present in the DOM.
+       * @see aria-posinset.
+       */
+      readonly "aria-setsize"?: AttributeValue<
+        Exclude<AttributeLiteral, boolean>
+      >;
+      /** Indicates if items in a table or grid are sorted in ascending or descending order. */
+      readonly "aria-sort"?: AttributeValue<
+        "none" | "ascending" | "descending" | "other"
+      >;
+      /** Defines the maximum allowed value for a range widget. */
+      readonly "aria-valuemax"?: AttributeValue<
+        Exclude<AttributeLiteral, boolean>
+      >;
+      /** Defines the minimum allowed value for a range widget. */
+      readonly "aria-valuemin"?: AttributeValue<
+        Exclude<AttributeLiteral, boolean>
+      >;
+      /**
+       * Defines the current value for a range widget.
+       * @see aria-valuetext.
+       */
+      readonly "aria-valuenow"?: AttributeValue<
+        Exclude<AttributeLiteral, boolean>
+      >;
+      /** Defines the human readable text alternative of aria-valuenow for a range widget. */
+      readonly "aria-valuetext"?: AttributeValue<string>;
+      role?: AttributeValue<
+        | "alert"
+        | "alertdialog"
+        | "application"
+        | "article"
+        | "banner"
+        | "button"
+        | "cell"
+        | "checkbox"
+        | "columnheader"
+        | "combobox"
+        | "complementary"
+        | "contentinfo"
+        | "definition"
+        | "dialog"
+        | "directory"
+        | "document"
+        | "feed"
+        | "figure"
+        | "form"
+        | "grid"
+        | "gridcell"
+        | "group"
+        | "heading"
+        | "img"
+        | "link"
+        | "list"
+        | "listbox"
+        | "listitem"
+        | "log"
+        | "main"
+        | "marquee"
+        | "math"
+        | "menu"
+        | "menubar"
+        | "menuitem"
+        | "menuitemcheckbox"
+        | "menuitemradio"
+        | "meter"
+        | "navigation"
+        | "none"
+        | "note"
+        | "option"
+        | "presentation"
+        | "progressbar"
+        | "radio"
+        | "radiogroup"
+        | "region"
+        | "row"
+        | "rowgroup"
+        | "rowheader"
+        | "scrollbar"
+        | "search"
+        | "searchbox"
+        | "separator"
+        | "slider"
+        | "spinbutton"
+        | "status"
+        | "switch"
+        | "tab"
+        | "table"
+        | "tablist"
+        | "tabpanel"
+        | "term"
+        | "textbox"
+        | "timer"
+        | "toolbar"
+        | "tooltip"
+        | "tree"
+        | "treegrid"
+        | "treeitem"
+      >;
     }
 
     interface SVGCommonAttributes<E extends globalThis.Element>
