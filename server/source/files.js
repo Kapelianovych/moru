badfaa7452367fd843085e7c9e4c2ec33b98e64d
Yevhen Kapelianovych <kapelianovych.y.v@protonmail.com>
2026-01-02 14:13:03 +0100

    Implement `files` handler with the `route` function.

--- server/source/files.js
+++ server/source/files.js
@@ -8,38 +8,47 @@ import { createReadStream, existsSync } from "node:fs";
 import mime from "mime";
 
 import { extractUrl } from "./request.js";
+import { HttpMethod, route } from "./route.js";
 
 /**
  * @param {string} directory
  * @return {RequestHandler}
  */
 export function files(directory) {
-  return (request, response, next) => {
-    const url = extractUrl(request);
-    let filePath = resolve(...directory.split("/"), ...url.pathname.split("/"));
+  return route({
+    path: "/",
+    method: HttpMethod.Get,
+    matchWholeUrl: false,
+    handler(request, response, next) {
+      const url = extractUrl(request);
+      let filePath = resolve(
+        ...directory.split("/"),
+        ...url.pathname.split("/"),
+      );
 
-    if (existsSync(filePath)) {
-      const mimeType = mime.getType(filePath);
-      const indexFilePath = join(filePath, "index.html");
+      if (existsSync(filePath)) {
+        const mimeType = mime.getType(filePath);
+        const indexFilePath = join(filePath, "index.html");
 
-      if (mimeType != null) {
-        response.statusCode = 200;
-        response.setHeader("content-type", mimeType);
-      } else if (existsSync(indexFilePath)) {
-        response.statusCode = 200;
-        response.setHeader("content-type", "text/html");
-        filePath = indexFilePath;
-      } else {
-        filePath = "";
-      }
+        if (mimeType != null) {
+          response.statusCode = 200;
+          response.setHeader("content-type", mimeType);
+        } else if (existsSync(indexFilePath)) {
+          response.statusCode = 200;
+          response.setHeader("content-type", "text/html");
+          filePath = indexFilePath;
+        } else {
+          filePath = "";
+        }
 
-      if (filePath.length > 0) {
-        createReadStream(filePath).pipe(response);
+        if (filePath.length > 0) {
+          createReadStream(filePath).pipe(response);
+        } else {
+          next();
+        }
       } else {
         next();
       }
-    } else {
-      next();
-    }
-  };
+    },
+  });
 }
